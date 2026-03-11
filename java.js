b3512a440ae3475ac7b01b74565269a3cbd240b2
Weiyi Z <WeiyiZhang3@cmail.carleton.ca>
2026-03-11 00:41:51 -0400

    Update java.js

--- java.js
+++ java.js
@@ -2,29 +2,30 @@
 const form = document.querySelector(".contact-form");
 const thankyou = document.querySelector(".thank-you");
 
-form.addEventListener("submit", function(event) {
-    event.preventDefault();
+if (form && thankYou) {
+    form.addEventListener("submit", function(event) {
+        event.preventDefault();
 
-    thankyou.style.display = "block";
-    form.reset();
+        thankYou.style.display = "block";
+        form.reset();
 
-    // hide message after 4 seconds
-    setTimeout(function() {
-        thankyou.style.display = "none";
-    }, 4000);
-});
+        setTimeout(function() {
+            thankYou.style.display = "none";
+        }, 4000);
+    });
+}
 
 
 /* --- HAMBURGER MENU SCRIPT --- */
 const hamburger = document.querySelector(".hamburger");
 const navLinks = document.querySelector(".nav-links");
 
-if (hamburger) {
+if (hamburger && navLinks) {
     hamburger.addEventListener("click", function() {
         navLinks.classList.toggle("active");
     });
+}
 
-};
 
 
 
